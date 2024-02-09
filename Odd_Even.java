b997fd6953afeac39e03cfb31347fef95a7da102
Amit Bhowmik <amitbhowmik407@gmail.com>
2024-02-09 20:54:16 +0600

    first commit

---
+++ Odd_Even.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Odd_Even {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int size;
+        System.out.print("Enter the size of array: ");
+        size = sc.nextInt();
+        int array[size];
+        for (int i = 0; i < size; i++) {
+            
+        }
+        sc.close();
+    }
+}
---
+++ factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int num = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        System.out.println("Factorial of " + num + " is: " + fact);
+        
+        sc.close();
+    }
+}
