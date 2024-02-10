f72150e452fa6cf9e7fc3bc4da25f1f8a76aa4a8
Amit Bhowmik <amitbhowmik407@gmail.com>
2024-02-10 19:35:36 +0600

    first commit

--- Odd_Even.java
+++ Odd_Even.java
@@ -6,10 +6,21 @@ public class Odd_Even {
         int size;
         System.out.print("Enter the size of array: ");
         size = sc.nextInt();
-        int array[size];
+        int[] array = new int[size];
         for (int i = 0; i < size; i++) {
-            
+            System.out.print("Index - " + i + " : ");
+            array[i] = sc.nextInt();
         }
+        int Even_sum = 0;
+        float avg = 0;
+        for (int i = 0; i < size; i++) {
+           if (array[i] % 2 == 0) {
+                Even_sum += array[i];
+                avg = (float)Even_sum / (float)size;
+           }
+        }
+        System.out.println("Sum of Even number : " + Even_sum);
+        System.out.println("Average of Even number : " + avg);
         sc.close();
     }
 }
